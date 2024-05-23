6fe129138a31503a200322b3f9e49c6a128a1681
Haijian Wang <130898843+haijian-wang@users.noreply.github.com>
2024-05-23 21:37:26 +0800

    Update Try.py

--- EartAgent/Try.py
+++ EartAgent/Try.py
@@ -1,5 +1,5 @@
 from EartAgent.Agent.text_agents import QwenAgent,AgentConfig
-QwenAgent.api_key = "sk-dfee314ae3734edeb7df40b6f64e6c83"
+QwenAgent.api_key = "your_api_key"
 agent_1 = QwenAgent(
     config=AgentConfig(name='Kerry',
                        system_prompt="",
