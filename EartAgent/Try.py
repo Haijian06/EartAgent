b0a65484e7a9073afcb0948219146b3688d2ac4a
Haijian Wang <130898843+Haijian06@users.noreply.github.com>
2024-07-22 19:40:09 +0800

    Delete EartAgent/Try.py

--- EartAgent/Try.py
+++
@@ -1,7 +0,0 @@
-from EartAgent.Agent.text_agents import QwenAgent,AgentConfig
-QwenAgent.api_key = "your_api_key"
-agent_1 = QwenAgent(
-    config=AgentConfig(name='Kerry',
-                       system_prompt="",
-                       ))
-agent_1("hi")
